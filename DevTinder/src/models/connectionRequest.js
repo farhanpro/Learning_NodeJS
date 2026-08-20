eabb88381a76f69ade12cf7132e66edd1e5a3c0c
farhanpro <shaikhfarhan549@gmail.com>
2026-08-21 01:46:34 +0530

    Writing logic

--- DevTinder/src/models/connectionRequest.js
+++ DevTinder/src/models/connectionRequest.js
@@ -17,6 +17,7 @@ const connectionRequestSchema = new mongoose.Schema({
     },
 },{timestamps:true}
 );
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 }, { unique: true });
 connectionRequestSchema.pre('save', function (next) {
     const connectionRequest = this;
     if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
